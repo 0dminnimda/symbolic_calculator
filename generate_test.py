e2f6d55a5d9919e0a29792391fb1fca6aa8e4338
0dminnimda <0dminnimda@gmail.com>
2024-05-05 01:00:23 +0300

    generate_test.py: add generate_equality() and tweak bounds

--- generate_test.py
+++ generate_test.py
@@ -22,8 +22,8 @@ def random_name(bounds: tuple[int, int] = (5, 25)) -> str:
 
 SPACE_BOUNDS = (0, 25)
 TERMS_BOUNDS = (0, 100)
-VAR_BOUNDS = (0, 100)
-PROD_BOUNDS = (0, 100)
+VAR_BOUNDS = (50, 100)
+PROD_BOUNDS = (50, 100)
 NUMBER_BOUNDS = (int(-1e-10), int(1e10))
 
 
@@ -33,6 +33,23 @@ products = list({
     tuple(sorted(random.choices(variables, k=random.randint(*TERMS_BOUNDS))))
     for _ in range(random.randint(*PROD_BOUNDS))
 })
+
+
+def print_coefficients(result: list[str], coefficients: list[int]):
+    for c, terms in zip(coefficients, products):
+        result.append(random_string(SPACE_BOUNDS, WHITESPACE))
+        if c == 0:
+            continue
+        result.append(f"{c:+}")
+        shuffled_terms = list(terms)
+        random.shuffle(shuffled_terms)
+        for term in shuffled_terms:
+            result.append(random_string(SPACE_BOUNDS, WHITESPACE))
+            result.append("*")
+            result.append(random_string(SPACE_BOUNDS, WHITESPACE))
+            result.append(term)
+    result.append("\n")
+
 
 def generate_add_sub(is_sub: bool):
     coefficients1 = [random.randint(*NUMBER_BOUNDS) for _ in range(len(products))]
@@ -50,21 +67,6 @@ def generate_add_sub(is_sub: bool):
     result.append("-" if is_sub else "+")
     result.append("\n")
 
-    def print_coefficients(result: list[str], coefficients: list[int]):
-        for c, terms in zip(coefficients, products):
-            result.append(random_string(SPACE_BOUNDS, WHITESPACE))
-            if c == 0:
-                continue
-            result.append(f"{c:+}")
-            shuffled_terms = list(terms)
-            random.shuffle(shuffled_terms)
-            for term in shuffled_terms:
-                result.append(random_string(SPACE_BOUNDS, WHITESPACE))
-                result.append("*")
-                result.append(random_string(SPACE_BOUNDS, WHITESPACE))
-                result.append(term)
-        result.append("\n")
-
     for coefficients in (coefficients1, coefficients2):
         print_coefficients(result, coefficients)
 
@@ -74,4 +76,27 @@ def generate_add_sub(is_sub: bool):
     return "".join(result)
 
 
-print(generate_add_sub(True))
+def generate_equality(equal: bool):
+    coefficients1 = [random.randint(*NUMBER_BOUNDS) for _ in range(len(products))]
+    if equal:
+        coefficients2 = coefficients1
+    else:
+        coefficients2 = [random.randint(*NUMBER_BOUNDS) for _ in range(len(products))]
+
+    result: list[str] = []
+    result.append("$$INPUT$$\n")
+    result.append("=")
+    result.append("\n")
+
+    for coefficients in (coefficients1, coefficients2):
+        print_coefficients(result, coefficients)
+
+    result.append("$$OUTPUT$$\n")
+    result.append("equal" if equal else "not equal")
+    result.append("\n")
+
+    return "".join(result)
+
+
+# print(generate_add_sub(True))
+print(generate_equality(True))
