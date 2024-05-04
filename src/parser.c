5d372e1f7df502b178728a2344dd6f5c551d827c
0dminnimda <0dminnimda@gmail.com>
2024-05-04 15:11:00 +0300

    parser.c: fix parser

--- src/parser.c
+++ src/parser.c
@@ -29,17 +29,23 @@ bool parse_long(Parser *parser, long *result) {
     long value = strtol(parser->str, &endptr, 10);
 
     if (endptr == parser->str) {
-        return true;
+#ifdef DEBUG_PARSER
+        printf("parse_long: didn't parse a thing\n");
+#endif
+        return false;
     }
     // if (endptr == NULL) {
     //     // fprintf(stderr, "Not an integer\n");
     //     return false;
     // }
     if (errno == ERANGE) {
-        // fprintf(stderr, "The number is out of range for a long\n");
+#ifdef DEBUG_PARSER
+        fprintf(stderr, "The number is out of range for a long\n");
+#endif
         return false;
     }
 
+    parser->str = endptr;
     *result = value;
     return true;
 }
@@ -51,7 +57,10 @@ bool parse_term(Parser *parser, Term *result) {
 
     const char *start = parser->str;
 
-    if (!isalnum(*parser->str) && *parser->str != '_') {
+    if (!isalpha(*parser->str) && *parser->str != '_') {
+#ifdef DEBUG_PARSER
+        printf("parse_term, first char is wrong\n");
+#endif
         return false;
     }
     ++parser->str;
@@ -60,6 +69,9 @@ bool parse_term(Parser *parser, Term *result) {
         if (!isalnum(*parser->str) && *parser->str != '_') break;
     }
 
+#ifdef DEBUG_PARSER
+    printf("parse_term, matched '%.*s'\n", (int)(parser->str - start), start);
+#endif
     result->variable_index = Variables_insert(&parser->sop->variables, start, parser->str - start);
 
     return true;
@@ -67,26 +79,42 @@ bool parse_term(Parser *parser, Term *result) {
 
 bool parse_product(Parser *parser, Product *result) {
     /*
-        product: long? term ('*' term)*
+        product: (long '*')? term ('*' term)*
     */
     parser->str = skip_whitespaces(parser->str);
     if (*parser->str == '\0') {
+#ifdef DEBUG_PARSER
+        printf("parse_product, found end of string\n");
+#endif
         return false;
     }
 
+    bool first = true;
     long coefficient = 1;
-    parse_long(parser, &coefficient);
+    if (parse_long(parser, &coefficient)) {
+        first = false;
+    }
+#ifdef DEBUG_PARSER
+    else {
+        printf("parse_product, no parse_long for '%s'\n", parser->str);
+    }
+#endif
     result->coefficient *= coefficient;
 
-    bool first = true;
     while (1) {
         parser->str = skip_whitespaces(parser->str);
         if (first) {
             if (*parser->str == '\0') {
+#ifdef DEBUG_PARSER
+                printf("parse_product: hit end of string\n");
+#endif
                 break;
             }
         } else {
             if (*parser->str != '*') {
+#ifdef DEBUG_PARSER
+                printf("parse_product: hit non '*' '%s'\n", parser->str);
+#endif
                 break;
             }
 
@@ -97,6 +125,9 @@ bool parse_product(Parser *parser, Product *result) {
         Term *term = malloc(sizeof(Term));
         Term_construct(term, -1, NULL);
         if (!parse_term(parser, term)) {
+#ifdef DEBUG_PARSER
+            printf("parse_product, bad Term\n");
+#endif
             Term_destruct(term);
             free(term);
             return false;
@@ -116,21 +147,32 @@ bool parse_sum_of_product(Parser *parser, SumOfProducts *result) {
     while (1) {
         parser->str = skip_whitespaces(parser->str);
         if (*parser->str == '\0') {
+#ifdef DEBUG_PARSER
+            printf("parse_product: hit end of string\n");
+#endif
             break;
         }
 
         long coefficient = 1;
         if (*parser->str == '-') {
             coefficient = -1;
+            ++parser->str;
         } else if (*parser->str == '+') {
             // coefficient = 1
+            ++parser->str;
         } else if (!first) {
+#ifdef DEBUG_PARSER
+            printf("parse_sum_of_product, not [+-] '%s'\n", parser->str);
+#endif
             return false;
         }
 
         Product *product = malloc(sizeof(Product));
         Product_construct(product, coefficient, NULL);
         if (!parse_product(parser, product)) {
+#ifdef DEBUG_PARSER
+            printf("parse_sum_of_product, bad Product\n");
+#endif
             Product_destruct(product);
             free(product);
             return false;
