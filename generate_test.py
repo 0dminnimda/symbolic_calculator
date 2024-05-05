5d025ad2d921c7f7ed2b1a51e2d39c0860ccdd6d
0dminnimda <0dminnimda@gmail.com>
2024-05-05 23:06:59 +0300

    generate_test.py: add abillity to generate multiplication

--- generate_test.py
+++ generate_test.py
@@ -19,20 +19,24 @@ def random_name(bounds: tuple[int, int] = (5, 25)) -> str:
     )
 
 
-SPACE_BOUNDS = (0, 25)
-NAME_BOUNDS = (5, 25)
-TERMS_BOUNDS = (0, 100)
-VAR_BOUNDS = (50, 100)
-PROD_BOUNDS = (50, 100)
-NUMBER_BOUNDS = (int(-1e-10), int(1e10))
+variables: list[str] = []
+products: list[tuple[str, ...]] = []
 
 
-variables = [random_name(NAME_BOUNDS) for _ in range(random.randint(*VAR_BOUNDS))]
+def generate_globals():
+    global variables, products
 
-products = list({
-    tuple(sorted(random.choices(variables, k=random.randint(*TERMS_BOUNDS))))
-    for _ in range(random.randint(*PROD_BOUNDS))
-})
+    variables = [random_name(NAME_BOUNDS) for _ in range(random.randint(*VAR_BOUNDS))]
+
+    products = list(
+        {
+            tuple(sorted(random.choices(variables, k=random.randint(*TERMS_BOUNDS))))
+            for _ in range(random.randint(*PROD_BOUNDS))
+        }
+    )
+    assert len(set(products)) == len(
+        products
+    ), "Got duplicate product, try again, good luck!"
 
 
 def print_coefficients(result: list[str], coefficients: list[int]):
@@ -51,10 +55,17 @@ def print_coefficients(result: list[str], coefficients: list[int]):
     result.append("\n")
 
 
+def generate_coeff(lower, upper):
+    # for coefficients we want 0 to be much more likely
+    if random.random() < ZERO_PROB:
+        return 0
+    return random.randint(lower, upper)
+
+
 def generate_add_sub(is_sub: bool):
-    coefficients1 = [random.randint(*NUMBER_BOUNDS) for _ in range(len(products))]
-    coefficients2 = [random.randint(*NUMBER_BOUNDS) for _ in range(len(products))]
-    result_coeffs = [0]*len(products)
+    coefficients1 = [generate_coeff(*NUMBER_BOUNDS) for _ in range(len(products))]
+    coefficients2 = [generate_coeff(*NUMBER_BOUNDS) for _ in range(len(products))]
+    result_coeffs = [0] * len(products)
 
     for i, (a, b) in enumerate(zip(coefficients1, coefficients2)):
         if is_sub:
@@ -76,11 +87,11 @@ def generate_add_sub(is_sub: bool):
 
 
 def generate_equality(equal: bool):
-    coefficients1 = [random.randint(*NUMBER_BOUNDS) for _ in range(len(products))]
+    coefficients1 = [generate_coeff(*NUMBER_BOUNDS) for _ in range(len(products))]
     if equal:
         coefficients2 = coefficients1
     else:
-        coefficients2 = [random.randint(*NUMBER_BOUNDS) for _ in range(len(products))]
+        coefficients2 = [generate_coeff(*NUMBER_BOUNDS) for _ in range(len(products))]
 
     input_s: list[str] = []
     input_s.append("=")
@@ -96,10 +107,67 @@ def generate_equality(equal: bool):
     return "".join(input_s), "".join(output_s)
 
 
+def generate_multiplication():
+    import sympy
+
+    var_map = dict(zip(variables, sympy.symbols(variables)))
+
+    sym_products = []
+    for product in products:
+        new_product = sympy.S.One
+        for var in product:
+            new_product *= var_map[var]
+        sym_products.append(new_product)
+
+    coefficients1 = [generate_coeff(*NUMBER_BOUNDS) for _ in range(len(products))]
+    coefficients2 = [generate_coeff(*NUMBER_BOUNDS) for _ in range(len(products))]
+    poly1 = sum((p * c for c, p in zip(coefficients1, sym_products)), sympy.S.Zero)
+    poly2 = sum((p * c for c, p in zip(coefficients2, sym_products)), sympy.S.Zero)
+
+    print("Multiplying")
+    result = sympy.expand(poly1 * poly2)
+
+    def expand_to_text(expr):
+        if isinstance(expr, sympy.Pow):
+            return "*".join([expand_to_text(expr.base)] * int(expr.exp))
+        elif isinstance(expr, sympy.Mul):
+            return '*'.join(expand_to_text(term) for term in expr.args)
+        elif isinstance(expr, sympy.Add):
+            return ' + '.join(expand_to_text(term) for term in expr.args)
+        else:
+            return str(expr)
+
+    print("Dumping strings")
+    input_s: list[str] = []
+    input_s.append("*")
+    input_s.append("\n")
+
+    for coefficients in (coefficients1, coefficients2):
+        print_coefficients(input_s, coefficients)
+
+    output_s: list[str] = []
+    output_s.append(expand_to_text(result))
+    output_s.append("\n")
+
+    return "".join(input_s), "".join(output_s)
+
+
 WHITESPACE = " "
+ZERO_PROB = 0.3
+SPACE_BOUNDS = (0, 4)
+NAME_BOUNDS = (3, 6)
+TERMS_BOUNDS = (20, 50)
+VAR_BOUNDS = (50, 70)
+PROD_BOUNDS = (50, 70)
+NUMBER_BOUNDS = (int(-1e-2), int(1e2))
+
+
+generate_globals()
+
 
 # input_s, output_s = generate_add_sub(True)
-input_s, output_s = generate_equality(True)
+# input_s, output_s = generate_equality(True)
+input_s, output_s = generate_multiplication()
 
 if len(sys.argv) <= 1:
     print(f"USAGE: {sys.argv[0]} <file>")
